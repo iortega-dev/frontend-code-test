b71768257d17b7347585c6f3bbc9197710e0519c
iortega-dev <iortegamancera@gmail.com>
2021-02-18 21:08:49 +0100

    feat: connect Toolbar component & implement box actions

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,14 +1,17 @@
-import React from "react";
+import { observer } from 'mobx-react';
+import React from 'react';
+import uuid from 'uuid';
+import getRandomColor from '../utils/getRandomColor';
 
-function Toolbar() {
-  return (
-    <div className="toolbar">
-      <button>Add Box</button>
-      <button>Remove Box</button>
-      <input type="color" />
-      <span>No boxes selected</span>
-    </div>
-  );
+function Toolbar(props) {
+	return (
+		<div className="toolbar">
+			<button onClick={() => props.store.addBox()}>Add Box</button>
+			<button onClick={() => props.store.removeBox()}>Remove Box</button>
+			<input type="color" />
+			<span>No boxes selected</span>
+		</div>
+	);
 }
 
-export default Toolbar;
+export default observer(Toolbar);
