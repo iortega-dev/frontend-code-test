76a52ea43a1cca0db13ea9fb971bb5d055c52ade
iortega-dev <iortegamancera@gmail.com>
2021-02-18 21:08:14 +0100

    feat: updated addBox & added removeBox methods

--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -1,30 +1,31 @@
-import { types } from "mobx-state-tree";
-import uuid from "uuid/v4";
-import BoxModel from "./models/Box";
-import getRandomColor from "../utils/getRandomColor";
+import { types } from 'mobx-state-tree';
+import uuid from 'uuid/v4';
+import BoxModel from './models/Box';
+import getRandomColor from '../utils/getRandomColor';
 
 const MainStore = types
-  .model("MainStore", {
-    boxes: types.array(BoxModel)
-  })
-  .actions(self => {
-    return {
-      addBox(box) {
-        self.boxes.push(box);
+	.model('MainStore', {
+		boxes: types.array(BoxModel),
+	})
+	.actions((self) => {
+		return {
+			addBox() {
+				self.boxes.push({
+					id: uuid(),
+					color: getRandomColor(),
+					left: 0,
+					top: 0,
+				});
+			},
+      removeBox() {
+        self.boxes.pop();
       }
-    };
-  })
-  .views(self => ({}));
+		};
+	})
+	.views((self) => ({}));
 
 const store = MainStore.create();
 
-const box1 = BoxModel.create({
-  id: uuid(),
-  color: getRandomColor(),
-  left: 0,
-  top: 0
-});
-
-store.addBox(box1);
+store.addBox()
 
 export default store;
