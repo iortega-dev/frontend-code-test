452def545a55046518b5c3ad2e6304656f2442d0
iortega-dev <iortegamancera@gmail.com>
2021-02-20 17:51:28 +0100

    feat: [BoxDraggable] added position register at Box

--- src/components/BoxDraggable.js
+++ src/components/BoxDraggable.js
@@ -1,14 +1,27 @@
-import React, { useEffect, useRef } from "react";
-import { observer } from "mobx-react";
-import interact from 'interactjs'
-
-import { dragMoveListener } from "../utils/dragMoveListener";
+import React, { useEffect, useRef } from 'react';
+import { observer } from 'mobx-react';
+import interact from 'interactjs';
+import { useCallback } from 'react';
 
 const BoxDraggable = (props) => {
+	const ref = useRef();
 
-  const ref = useRef();
+	const dragMoveListener = useCallback((event) => {
+		var target = event.target;
+		// keep the dragged position in the data-x/data-y attributes
+		var x = (parseFloat(props.box.left) || 0) + event.dx;
+		var y = (parseFloat(props.box.top) || 0) + event.dy;
 
-  useEffect(() => {
+		// translate the element
+		target.style.webkitTransform = target.style.transform =
+			'translate(' + x + 'px, ' + y + 'px)';
+
+		// update the posiion attributes
+		props.box.setLeft(x);
+		props.box.setTop(y);
+	}, [props.box]);
+
+	useEffect(() => {
 		interact(ref.current).draggable({
 			modifiers: [
 				interact.modifiers.restrictRect({
@@ -20,24 +33,24 @@ const BoxDraggable = (props) => {
 				move: dragMoveListener,
 			},
 		});
-	}, [ref]);
+	}, [dragMoveListener, ref]);
 
-  return (
-    <div
-      ref={ref}
-      id={props.id}
-      className={`box pointer ${props.box.selected ? 'selected' : ''}`}
-      onClick={props.box.toggleSelected}
-      style={{
-        backgroundColor: props.color,
-        width: props.width,
-        height: props.height,
-        transform: `translate(${props.left}px, ${props.top}px)`
-      }}
-    >
-      {props.children}
-    </div>
-  );
+	return (
+		<div
+			ref={ref}
+			id={props.id}
+			className={`box pointer ${props.box.selected ? 'selected' : ''}`}
+			onClick={props.box.toggleSelected}
+			style={{
+				backgroundColor: props.color,
+				width: props.width,
+				height: props.height,
+				transform: `translate(${props.left}px, ${props.top}px)`,
+			}}
+		>
+			{props.children}
+		</div>
+	);
 };
 
 export default observer(BoxDraggable);
