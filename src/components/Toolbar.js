93c6efa1a04ecfc5340132b8c27895938c42169f
iortega-dev <iortegamancera@gmail.com>
2021-02-20 19:01:19 +0100

    feat: [Toolbar] added change color of selected boxes

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,17 +2,22 @@ import { observer } from 'mobx-react';
 import React from 'react';
 
 function Toolbar(props) {
+
+	const changeSelectedBoxesColor = (color) => {
+		props.store.selectedBoxes.forEach((box) => box.changeColor(color))
+	}
+
 	return (
 		<div className="toolbar">
 			<button onClick={() => props.store.addBox()}>Add Box</button>
 			<button onClick={() => props.store.removeBox()}>Remove Box</button>
 			<input
 				type="color"
-				disabled={!props.store.selectedBox}
+				disabled={!props.store.selectedBoxes.length > 0}
 				value={
-					props.store.selectedBox ? props.store.selectedBox.color : '#000000'
+					props.store.selectedBoxes.length > 0 ? props.store.selectedBoxes[props.store.selectedBoxes.length - 1].color : '#000000'
 				}
-				onChange={(event) => props.store.selectedBox.changeColor(event.target.value)}
+				onChange={(event) => changeSelectedBoxesColor(event.target.value)}
 			/>
 			<span>{props.store.selectedBoxes.length > 0 ? `Selected ${props.store.selectedBoxes?.length} boxes` : 'No boxes selected'}</span>
 		</div>
