409f8cca3a014a06bced110806c5e49139512268
iortega-dev <iortegamancera@gmail.com>
2021-02-20 19:51:21 +0100

    feat: [Toolbar] added undo/redo buttons to toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,26 +1,43 @@
 import { observer } from 'mobx-react';
 import React from 'react';
 
-function Toolbar(props) {
+import { undoManager } from '../stores/MainStore'
 
+function Toolbar(props) {
 	const changeSelectedBoxesColor = (color) => {
-		props.store.selectedBoxes.forEach((box) => box.changeColor(color))
-	}
+		props.store.selectedBoxes.forEach((box) => box.changeColor(color));
+	};
+
+	const undo = () => undoManager.canUndo && undoManager.undo()
+	const redo = () => undoManager.canRedo && undoManager.redo()
 
 	return (
-		<div className="toolbar">
-			<button onClick={() => props.store.addBox()}>Add Box</button>
-			<button onClick={() => props.store.removeBox()}>Remove Box</button>
-			<input
-				type="color"
-				disabled={!props.store.selectedBoxes.length > 0}
-				value={
-					props.store.selectedBoxes.length > 0 ? props.store.selectedBoxes[props.store.selectedBoxes.length - 1].color : '#000000'
-				}
-				onChange={(event) => changeSelectedBoxesColor(event.target.value)}
-			/>
-			<span>{props.store.selectedBoxes.length > 0 ? `Selected ${props.store.selectedBoxes?.length} boxes` : 'No boxes selected'}</span>
-		</div>
+		<>
+			<div className="toolbar">
+				<button onClick={() => props.store.addBox()}>Add Box</button>
+				<button onClick={() => props.store.removeBox()}>Remove Box</button>
+				<input
+					type="color"
+					disabled={!props.store.selectedBoxes.length > 0}
+					value={
+						props.store.selectedBoxes.length > 0
+							? props.store.selectedBoxes[props.store.selectedBoxes.length - 1]
+									.color
+							: '#000000'
+					}
+					onChange={(event) => changeSelectedBoxesColor(event.target.value)}
+				/>
+				<span>
+					{props.store.selectedBoxes.length > 0
+						? `Selected ${props.store.selectedBoxes?.length} boxes`
+						: 'No boxes selected'}
+				</span>
+			</div>
+			<div className="toolbar-history">
+				<button onClick={() => undo()}>Undo</button>
+				<button onClick={() => redo()}>Redo</button>
+			</div>
+		</>
 	);
 }
 
